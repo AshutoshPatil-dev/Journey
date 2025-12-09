b0c03aefcbb5424536d98705a50c186b8099172c
AshutoshPatil-dev <ashutosh.patil9750@gmail.com>
2025-12-09 18:11:03 +0530

    Switched places of q10 and q9

--- C Language/College/Practicals/Q09.c
+++ C Language/College/Practicals/Q09.c
@@ -1,26 +1,29 @@
+/*9.	Write a program for bubble sort on an array of integers.*/
+
 #include<stdio.h>
-  
-
-long long iter_func(long long a){
-  long long fact = 1;
-  for(long long i = 1; i<=a; i++){
-    fact = fact * i;
-  }
-  return fact;
-}
-
-long long rec_func(long long a){
- if(a == 1 || a == 0)
-  return 1;
- return a * rec_func(a-1);
-}
-
 int main(){
-long long a;
-printf("Enter a number: ");
-scanf("%lld", &a);
+int i, j, n, temp, arr[100];
 
-printf("%lld\n", iter_func(a));
-
-printf("%lld\n", rec_func(a));
+printf("Enter number of elements in the array: ");
+scanf("%d", &n);
+printf("Enter the elements of array: ");
+for(i = 0; i < n; i++){
+    scanf("%d", &arr[i]);
 }
+
+for(i = 0; i < n-1; i++){
+    for(j = 0; j < n - 1 - i; j++){
+     if(arr[j] > arr[j+1]){
+        temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+
+     }
+    }
+}
+printf("Sorted array: ");
+for(i = 0; i < n; i++){
+    printf("%d ", arr[i]);
+}
+}
+ 
--- C Language/College/Practicals/q10.c
+++ C Language/College/Practicals/q10.c
@@ -1,27 +1,26 @@
+/*10. Write programs to calculate the factorial of a number entered by the user, 
+using both an iterative function and a recursive function*/
 #include<stdio.h>
+long long iter_func(long long a){
+  long long fact = 1;
+  for(long long i = 1; i<=a; i++){
+    fact = fact * i;
+  }
+  return fact;
+}
+
+long long rec_func(long long a){
+ if(a == 1 || a == 0)
+  return 1;
+ return a * rec_func(a-1);
+}
+
 int main(){
-int i, j, n, temp, arr[100];
+long long a;
+printf("Enter a number: ");
+scanf("%lld", &a);
 
-printf("Enter number of elements in the array: ");
-scanf("%d", &n);
-printf("Enter the elements of array: ");
-for(i = 0; i < n; i++){
-    scanf("%d", &arr[i]);
-}
+printf("%lld\n", iter_func(a));
 
-for(i = 0; i < n-1; i++){
-    for(j = 0; j < n - 1 - i; j++){
-     if(arr[j] > arr[j+1]){
-        temp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j+1] = temp;
-
-     }
-    }
+printf("%lld\n", rec_func(a));
 }
-printf("Sorted array: ");
-for(i = 0; i < n; i++){
-    printf("%d ", arr[i]);
-}
-}
- 
