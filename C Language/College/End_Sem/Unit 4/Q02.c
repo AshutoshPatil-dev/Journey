44e7c1570707e49b05a720ebbfbb6abdad78d382
AshutoshPatil-dev <ashutosh.patil9750@gmail.com>
2025-12-18 10:42:47 +0530

    Added end sem prep file

--- C Language/College/End_Sem/Unit 4/Q02.c
+++ C Language/College/End_Sem/Unit 4/Q02.c
@@ -1,4 +1,27 @@
-/*Q2. Explain the difference between call by value and call by reference with a 
-  simple example for swapping two numbers.*/
+#include<stdio.h>
 
-  
+
+void callbyvalue(int a,int b){
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+void callbyref(int *a, int *b){
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main(){
+ int a = 1, b = 2;
+ callbyvalue(a, b);
+ printf("%d %d\n", a, b);
+
+ callbyref(&a, &b);
+ printf("%d %d", a, b);
+}
+
+
