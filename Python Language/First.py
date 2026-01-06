014273bb9750dd0b45b3ded54bbaf1a603c4b1ee
Ashutosh Patil <92073339+AshutoshPatil-dev@users.noreply.github.com>
2026-01-06 12:44:40 +0530

    First python program

---
+++ Python Language/First.py
@@ -0,0 +1,27 @@
+'''print("Hello World")'''
+
+n = str(input("Enter your name: "))
+a = int(input("Enter marks of subject 1: "))
+b = int(input("Enter marks of subject 2: "))
+c = int(input("Enter marks of subject 3: "))
+d = int(input("Enter marks of subject 4: "))
+e = int(input("Enter marks of subject 5: "))
+agg = a+b+c+d+e
+perc = agg/500*100
+print("Name: ", n)
+print("Aggeregate: " ,agg)
+print("Percentage: ",perc)
+
+'''
+bsal = float(input("Enter basic salary: "))
+da = 0.5*bsal
+hra = 0.2*bsal
+gsal = bsal + da + hra
+
+print("Gross Salary: ", gsal)
+'''
+'''
+output:
+Enter basic salary: 700.2
+Gross Salary:  1190.3400000000001
+'''
